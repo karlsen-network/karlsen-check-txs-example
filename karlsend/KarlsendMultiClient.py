597a648528d439a242ade124e96859784aeb65c9
Lemois 1337 <lemois1337@protonmail.com>
2024-01-17 00:13:48 +0100

    Added support for Karlsen Network

--- karlsend/KarlsendMultiClient.py
+++ karlsend/KarlsendMultiClient.py
@@ -1,32 +1,32 @@
 # encoding: utf-8
 import asyncio
 
-from kaspad.KaspadClient import KaspadClient
+from karlsend.KarlsendClient import KarlsendClient
 # pipenv run python -m grpc_tools.protoc -I./protos --python_out=. --grpc_python_out=. ./protos/rpc.proto ./protos/messages.proto ./protos/p2p.proto
-from kaspad.KaspadThread import KaspadCommunicationError
+from karlsend.KarlsendThread import KarlsendCommunicationError
 
 
-class KaspadMultiClient(object):
+class KarlsendMultiClient(object):
     def __init__(self, hosts: list[str]):
-        self.kaspads = [KaspadClient(*h.split(":")) for h in hosts]
+        self.karlsends = [KarlsendClient(*h.split(":")) for h in hosts]
 
-    def __get_kaspad(self):
-        for k in self.kaspads:
+    def __get_karlsend(self):
+        for k in self.karlsends:
             if k.is_utxo_indexed and k.is_synced:
                 return k
 
     async def initialize_all(self):
-        tasks = [asyncio.create_task(k.ping()) for k in self.kaspads]
+        tasks = [asyncio.create_task(k.ping()) for k in self.karlsends]
 
         for t in tasks:
             await t
 
     async def request(self, command, params=None, timeout=60):
         try:
-            return await self.__get_kaspad().request(command, params, timeout=timeout, retry=1)
-        except KaspadCommunicationError:
+            return await self.__get_karlsend().request(command, params, timeout=timeout, retry=1)
+        except KarlsendCommunicationError:
             await self.initialize_all()
-            return await self.__get_kaspad().request(command, params, timeout=timeout, retry=3)
+            return await self.__get_karlsend().request(command, params, timeout=timeout, retry=3)
 
     async def notify(self, command, params, callback):
-        return await self.__get_kaspad().notify(command, params, callback)
+        return await self.__get_karlsend().notify(command, params, callback)
